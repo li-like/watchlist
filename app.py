64eda1c90b85566da065943efe4627fa57d9ba82
Hongbao Li <208039302@qq.com>
2023-03-28 20:49:24 +0800

    Add database support with Flask-SQLAlchemy

---
+++ app.py
@@ -0,0 +1,69 @@
+from flask import Flask,render_template,redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+import click
+
+app = Flask(__name__)
+# 设置MySQL所在的主机名
+HOSTNAME = "127.0.0.1"
+# MYSQL监听的端口号
+PORT = 3306
+# 连接用户名和密码
+USERNAME = "root"
+PASSWORD = "root"
+DATABASE = "database_zhiliao"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4"
+
+db = SQLAlchemy(app)
+
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 全局的两个变量移动到这个函数内
+    name = "李洪宝"
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year': '1988'},
+        {'title': 'Dead Poets Society', 'year': '1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])#创建Movie表的实例
+        db.session.add(movie)
+
+    db.session.commit()
+    click.echo('Done.')
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+@app.route('/')
+def index():
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', user=user, movies=movies)
+
+
+if __name__ == '__main__':
+    app.run()
