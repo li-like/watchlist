b0b77676970c8c4d29a0ea507cc2d1a112ba2488
Hongbao Li <208039302@qq.com>
2023-03-28 22:41:43 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect
+from flask import Flask, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 import click
@@ -13,7 +13,8 @@ USERNAME = "root"
 PASSWORD = "root"
 DATABASE = "database_zhiliao"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4"
+app.config[
+    'SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}?charset=utf8mb4"
 
 db = SQLAlchemy(app)
 
@@ -41,11 +42,12 @@ def forge():
     user = User(name=name)
     db.session.add(user)
     for m in movies:
-        movie = Movie(title=m['title'], year=m['year'])#创建Movie表的实例
+        movie = Movie(title=m['title'], year=m['year'])  # 创建Movie表的实例
         db.session.add(movie)
 
     db.session.commit()
     click.echo('Done.')
+
 
 class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
     id = db.Column(db.Integer, primary_key=True)  # 主键
@@ -62,8 +64,18 @@ class Movie(db.Model):  # 表名将会是 movie
 def index():
     user = User.query.first()  # 读取用户记录
     movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
+    return render_template('index.html', movies=movies)
 
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    user = User.query.first()
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+@app.context_processor#上下文处理函数
+def inject_user():  # 函数名可以随意修改
+    user = User.query.first()
+    return dict(user=user)  # 需要返回字典，等同于 return {'user': user}
 
 if __name__ == '__main__':
     app.run()
